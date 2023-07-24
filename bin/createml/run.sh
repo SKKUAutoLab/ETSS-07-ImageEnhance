6c3a851b2d91aae607ad9d0c141863584b2f4801
longpham <longpham3105@gmail.com>
2023-07-24 12:41:09 +0900

    Edit training script

--- bin/createml/run.sh
+++ bin/createml/run.sh
@@ -23,23 +23,22 @@ if [ "$task" == "train" ]; then
   echo -e "\nTraining"
   if [ "$machine" == "lp-labdesktop-01-ubuntu" ]; then
     python train.py \
-      --config "zerodcev2_lol226" \
+      --config "zerodcev2_a_lol" \
       --root "${root_dir}/run/train" \
       --project "zerodcev2" \
-      --name "zerodcev2-lol226" \
-      --batch-size 4 \
+      --name "zerodcev2-a-lol" \
+      --batch-size 8 \
       --image-size 512 \
       --accelerator "auto" \
       --devices 0 \
-      --max-epochs 200 \
-      --max-steps "NULL" \
+      --max-epochs 20 \
       --strategy "auto"
   elif [ "$machine" == "lp-labdesktop-02-ubuntu" ]; then
     python train.py \
-      --config "zerodcev2_b_lol" \
+      --config "zerodcev2_c_lol" \
       --root "${root_dir}/run/train" \
       --project "zerodcev2" \
-      --name "zerodcev2-b-lol" \
+      --name "zerodcev2-c-lol" \
       --batch-size 2 \
       --image-size 128 \
       --accelerator "auto" \
@@ -57,7 +56,6 @@ if [ "$task" == "train" ]; then
       --accelerator "auto" \
       --devices 0 \
       --max-epochs 100 \
-      --max-steps "NULL" \
       --strategy "auto"
   elif [ "$machine" == "vsw-ws02" ]; then
     python train.py \
@@ -70,9 +68,8 @@ if [ "$task" == "train" ]; then
       --image-size 256 256 \
       --accelerator "auto" \
       --devices 1 \
-      --max-epochs "NULL" \
-      --max-steps "NULL" \
-      --strategy "NULL"
+      --max-epochs 100 \
+      --strategy "auto"
   elif [ "$machine" == "vsw-ws03" ]; then
     python train.py \
       --config "hinet_rain13k" \
@@ -84,9 +81,8 @@ if [ "$task" == "train" ]; then
       --image-size 256 256 \
       --accelerator "auto" \
       --devices 1 \
-      --max-epochs "NULL" \
-      --max-steps "NULL" \
-      --strategy "NULL"
+      --max-epochs 100 \
+      --strategy "auto"
   fi
 fi
 
